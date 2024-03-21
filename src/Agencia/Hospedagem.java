bc7436691a3214d7aadb309b62b0477d7a01c555
Lucas <lucasgabriel.1234@hotmail.com>
2024-03-21 16:53:49 -0300

    Modificações

--- src/Agencia/Hospedagem.java
+++ src/Agencia/Hospedagem.java
@@ -1,4 +1,94 @@
 package Agencia;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Hospedagem {
+
+    private Hotel hotel1;
+    private ArrayList<Hotel> hoteis;
+    private LocalDate checkin;
+    private LocalDate checkout;
+    private float diasEstadia;
+    private double precoDiaria;
+
+    private boolean cancelarReserva;
+
+
+    public Hospedagem(LocalDate checkin, LocalDate checkout, float diasEstadia, double precoDiaria) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+        this.diasEstadia = diasEstadia;
+        this.precoDiaria = precoDiaria;
+        this.cancelarReserva = false;
+    }
+
+    public Hospedagem() {
+
+        this.hoteis = new ArrayList<>();
+    }
+
+    public boolean validarHospedagem(){
+        if (checkout.isAfter(checkin)){
+            System.out.println("Parabens, hospedagem no hotel " + hotel.getNomeHotel() +"!");
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void cancelar(){
+        cancelarReserva = true;
+    }
+    public boolean isReservaCancelada(){
+        return cancelarReserva;
+    }
+
+    public float calcularEstadia(){
+
+        diasEstadia = ChronoUnit.DAYS.between(checkin, checkout);
+        return diasEstadia;
+    }
+
+
+    public double calcularPrecoEstadia(){
+        double x;
+        x = calcularEstadia() * precoDiaria;
+        return x;
+    }
+
+    public void adicionarHotel(Hotel hotel) {
+        hoteis.add(hotel);
+    }
+
+    public Hotel consultarHotel(String nomeHotel) {
+        for (Hotel hotel : hoteis) {
+            if (hotel.getNomeHotel().equals(nomeHotel)) {
+                return hotel;
+            }
+        }
+        return null; // Retorna null se o hotel não for encontrado
+    }
+
+    public String relatorio(){
+        String x = "";
+        String y = "";
+
+
+
+        x = "Nome do Hotel: " +  +"\nData Checkin: " + checkin + "\nData Checkout: " + checkout +
+                "\nPreço Hospedagem: " + calcularEstadia() + "\nReserva ativa: " + cancelarReserva;
+
+
+        return y + x;
+
+    }
+
+
+
+
+
 }
--- src/Agencia/Principal.java
+++ src/Agencia/Principal.java
@@ -1,2 +1,190 @@
-package Agencia;public class Principal {
+package Agencia;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int op = 0, op1 = 0, op2 = 0;
+
+        do {
+            System.out.println("=========Menu=========");
+            System.out.println("1 - Login");
+            System.out.println("2 - Cadastro");
+            System.out.println("0 - Sair");
+            System.out.print("Opcao: ");
+            op = sc.nextInt();
+            sc.nextLine();
+
+            switch (op){
+                case 1:
+                    if(loginCliente()){
+                        do {
+                        System.out.println("=========Menu=========");
+                        System.out.println("1 - Nova Viagem");
+                        System.out.println("2 - Histórico de Viagens");
+                        System.out.println("0 - Sair");
+                        System.out.print("Opcao: ");
+                        op1 = sc.nextInt();
+                        sc.nextLine();
+
+                        switch (op1){
+                            case 1:
+                                System.out.println("Que bom que escolheu planejar sua viagem com a gente!");
+                                System.out.println("Qual data deseja viajar? (Formato DD/MM/YYYY)");
+                                String dataString = sc.nextLine();
+
+                                DateTimeFormatter formatar = DateTimeFormatter.ofPattern("DD/MM/YYYY");
+                                try {
+                                    LocalDate data = LocalDate.parse(dataString, formatar);
+                                    System.out.println("Data lida: " + data.format(formatar));
+                                } catch (Exception e) {
+                                    System.out.println("Formato de data inválido. Certifique-se de que está no formato (DD/MM/YYYY).");
+                                }
+
+                                System.out.println("Ótima data! Temos os seguintes destinos disponíveis: ");
+                                System.out.println("1 - Paris");
+                                System.out.println("2 - Estados Unidos");
+                                System.out.println("3 - Espanha");
+                                System.out.println("4 - Canadá");
+                                System.out.println("5 - Grécia");
+                                op2 = sc.nextInt();
+                                sc.nextLine();
+
+                                switch (op2){
+                                    case 1:
+                                        System.out.println("Excelente destino! Temos as seguintes opções de Pacotes");
+                                        System.out.println("1 - Pacote A: Roteiro: Hotel A; Atrações A; Tantos dias");
+                                        System.out.println("3 - Pacote B: Roteiro: Hotel B; Atrações B; Tantos dias");
+                                        System.out.println("4 - Pacote C: Roteiro: Hotel C; Atrações C; Tantos dias");
+                                        break;
+                                    case 2:
+                                        System.out.println("Excelente destino! Temos as seguintes opções de Pacotes");
+                                        System.out.println("1 - Pacote A: Roteiro: Hotel A; Atrações A; Tantos dias");
+                                        System.out.println("3 - Pacote B: Roteiro: Hotel B; Atrações B; Tantos dias");
+                                        System.out.println("4 - Pacote C: Roteiro: Hotel C; Atrações C; Tantos dias");
+                                        break;
+                                    case 3:
+                                        System.out.println("Excelente destino! Temos as seguintes opções de Pacotes");
+                                        System.out.println("1 - Pacote A: Roteiro: Hotel A; Atrações A; Tantos dias");
+                                        System.out.println("3 - Pacote B: Roteiro: Hotel B; Atrações B; Tantos dias");
+                                        System.out.println("4 - Pacote C: Roteiro: Hotel C; Atrações C; Tantos dias");
+                                        break;
+                                    case 4:
+                                        System.out.println("Excelente destino! Temos as seguintes opções de Pacotes");
+                                        System.out.println("1 - Pacote A: Roteiro: Hotel A; Atrações A; Tantos dias");
+                                        System.out.println("3 - Pacote B: Roteiro: Hotel B; Atrações B; Tantos dias");
+                                        System.out.println("4 - Pacote C: Roteiro: Hotel C; Atrações C; Tantos dias");
+                                        break;
+                                    case 5:
+                                        System.out.println("Excelente destino! Temos as seguintes opções de Pacotes");
+                                        System.out.println("1 - Pacote A: Roteiro: Hotel A; Atrações A; Tantos dias");
+                                        System.out.println("3 - Pacote B: Roteiro: Hotel B; Atrações B; Tantos dias");
+                                        System.out.println("4 - Pacote C: Roteiro: Hotel C; Atrações C; Tantos dias");
+                                        break;
+                                }
+
+
+                                break;
+                        }
+
+                    }while (op1 != 0);
+                    }
+                    break;
+                case 2:
+                    cadastraCliente();
+
+                    break;
+            }
+
+        }while(op != 0);
+
+
+
+
+
+
+
+    }
+
+    public static boolean cadastraCliente(){
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Digite seu nome completo: ");
+            String nome = sc.next();
+            System.out.print("Digite seu email: ");
+            String email = sc.next();
+            System.out.print("Digite sua senha: ");
+            String senha = sc.next();
+
+            System.out.println("Obrigado!");
+
+            String print = nome + ";" + email + ";" + senha;
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter("Clientes.txt"))) {
+                writer.write(print);
+            } catch (IOException e) {
+                System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
+            }
+
+            return true;
+        }catch (Exception ex){
+            System.out.println("ERRO! TENTE NOVAMENTE");
+            sc.nextLine();
+            return false;
+        }
+    }
+
+    public static boolean loginCliente(){
+        Scanner sc = new Scanner(System.in);
+
+        try {
+        System.out.print("Digite seu email: ");
+        String email = sc.next();
+        System.out.print("Digite sua senha: ");
+        String senha = sc.next();
+
+        if(verificaCredencias(email, senha)){
+            System.out.println("Login Efetuado!");
+            return true;
+        }
+        }catch (Exception e){
+            System.out.println("ERRO! TENTE NOVAMENTE");
+            sc.nextLine();
+            return false;
+        }
+
+
+        return false;
+
+    }
+
+    public static boolean verificaCredencias(String email, String senha) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("Clientes.txt"))) {
+            String linha; // usada para armazenar cada linha lida
+            while ((linha = br.readLine()) != null) {//le cada linha do arquivo até que nao haja mais linhas
+                String[] partes = linha.split(";"); //le até o ";" e armazena em uma array chamadas partes
+                String emailSalvo = partes[1]; // salva a segunda parte na string que é o email
+                String senhaSalva = partes[2]; // salva a terceira parte na string que é a senha
+                if (email.equals(emailSalvo) && senha.equals(senhaSalva)) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
+
