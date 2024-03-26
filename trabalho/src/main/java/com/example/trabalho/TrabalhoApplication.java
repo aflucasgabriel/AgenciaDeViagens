9be6c76d3ab2a4e8ecc2f641d590397339215258
Laís Silva <109741712+laisso@users.noreply.github.com>
2024-03-26 15:56:52 -0300

    Add files via upload

---
+++ trabalho/src/main/java/com/example/trabalho/TrabalhoApplication.java
@@ -0,0 +1,16 @@
+package com.example.trabalho;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class TrabalhoApplication {
+
+	public static void main(String[] args) {
+
+		
+
+
+	}
+
+}
